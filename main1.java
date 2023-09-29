c6dcc82df6733012f929a13d6fd568023ddbe9c7
Chris Sheridan <csheri46@gmail.com>
2023-09-29 13:26:22 -0400

    Added basic gameplay. Enemy now has preset battlefield, randomly picks a space to attack.

--- main1.java
+++ main1.java
@@ -11,6 +11,7 @@ public class main1 {
     static int x, y;
     static String A;
     static int[][] battleField = new int [row][column];
+    static int[][] enemyBattleField = new int [row][column];
 
 
 
@@ -18,6 +19,30 @@ public class main1 {
         for(int i=0; i<10; i++){
             for(int j=0; j<10; j++){
                 System.out.print(battleField[j][i] +" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void fillenemyBattleField(){
+        enemyBattleField [1][1] = 1;
+        enemyBattleField [2][1] = 1;
+        enemyBattleField [7][2] = 1;
+        enemyBattleField [7][3] = 1;
+        enemyBattleField [7][4] = 1;
+        enemyBattleField [5][9] = 1;
+        enemyBattleField [6][9] = 1;
+        enemyBattleField [7][9] = 1;
+        enemyBattleField [8][9] = 1;
+        enemyBattleField [1][4] = 1;
+        enemyBattleField [1][5] = 1;
+        enemyBattleField [1][6] = 1;
+        enemyBattleField [1][7] = 1;
+        enemyBattleField [1][8] = 1;
+        System.out.print("Enemy Board: \n");
+        for(int i=0; i<10; i++){
+            for(int j=0; j<10; j++){
+                System.out.print(enemyBattleField[j][i] +" ");
             }
             System.out.println();
         }
@@ -125,7 +150,14 @@ public class main1 {
 
     public static void main(String[] args){
 
-       printBoard();
+       boolean wl = false;
+       int xCoord, yCoord;
+       Random rand = new Random();
+       int rMax = 9;
+       int rMin = 0;
+       int playerRemainingHits = 14;
+       int enemyRemainingHits = 14;
+        printBoard();
 
        twoShip(x,y,A);
        threeShip(x,y,A);
@@ -133,6 +165,43 @@ public class main1 {
        fiveShip(x,y,A);
 
        printBoard();
+       fillenemyBattleField();
+       while (!wl){
+           System.out.println("Enter x coordinate (0-9): ");
+           xCoord = sc.nextInt();
+           System.out.println("Enter Y Coordinate(0-9): ");
+           yCoord = sc.nextInt();
+           if (enemyBattleField[xCoord][yCoord] == 1){
+               enemyBattleField[xCoord][yCoord] = 9;
+               System.out.println("Hit!");
+               enemyRemainingHits -= 1;
+           }
+           else {
+               System.out.println("Miss!");
+           }
+           if (enemyRemainingHits == 0){
+               System.out.println("All enemy ships sunk! You win!");
+               wl = true;
+           }
+           if (!wl){
+               xCoord = rand.nextInt(rMax-rMin) + 1;
+               yCoord = rand.nextInt(rMax-rMin) + 1;
+           }
+           if (battleField [xCoord][yCoord] == 1){
+               battleField [xCoord][yCoord] = 9;
+               System.out.println("Enemy attacked [" + xCoord + "], [" + yCoord + "], Hit!");
+               playerRemainingHits -= 1;
+           }
+           else{
+               System.out.println("Enemy attacked [" + xCoord + "], [" + yCoord + "], Miss!");
+           }
+           if (playerRemainingHits == 0){
+               System.out.println("All ships sunk! You lose!");
+               wl = true;
+           }
+
+
+       }
 
 
 
